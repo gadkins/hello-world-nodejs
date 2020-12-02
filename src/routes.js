7d36cf5b1fac2572b35028973516941a1cc68400
Grayson Adkins <grayson@idyl.tech>
2020-12-02 15:30:55 -0600

    new wording

--- src/routes.js
+++ src/routes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const appRouter = express.Router();
 
 appRouter.get('/*', function (req, res) {
-  res.send(`<h1>BradCerasuolo.com 
+  res.send(`<h1>BradCerasuolo.com \n
   A site dedicated to how lame Brad is. </h1>
   `);
 });
