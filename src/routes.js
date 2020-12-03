c1ce009ce91ec064470a402ac2229d6a06a2f530
Grayson Adkins <grayson@idyl.tech>
2020-12-03 13:05:09 -0600

    rewording

--- src/routes.js
+++ src/routes.js
@@ -2,9 +2,7 @@ const express = require('express');
 const appRouter = express.Router();
 
 appRouter.get('/*', function (req, res) {
-  res.send(`<h1>BradCerasuolo.com \n
-  A site dedicated to how lame Brad is. </h1>
-  `);
+  res.send('<h1>Hello, World! </h1>');
 });
 
 module.exports = appRouter;
